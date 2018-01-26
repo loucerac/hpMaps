2d1e0c60e5e34a26a00c07ca9acf23e7b492ba9f
Carlos <loucerac@unican.es>
2018-01-26 10:19:37 +0100

    code cleanup

--- rdiseasemaps/scr01.R
+++ rdiseasemaps/scr01.R
@@ -10,13 +10,14 @@ biopax_filepath <- normalizePath(
 )
 
 sif <- toSif(inputFile=biopax_filepath)
+# convert to igraph
 sif_graph <- graph.edgelist(as.matrix(sif[, c(1, 3)]), directed = T)
 
 # rank-order distribution
 degree_in <- degree(sif_graph, mode = "in")
-start_nodes <- names(degree_in[degree_in==0])
+start_nodes <- names(degree_in[degree_in == 0])
 degree_out <- degree(sif_graph, mode = "out")
-terminal_nodes <- names(degree_out[degree_out==0])
+terminal_nodes <- names(degree_out[degree_out == 0])
 isolated_nodes <- intersect(start_nodes, terminal_nodes)
 
 cat("Isolated nodes: ", isolated_nodes)
