ab1c27a33e427e6d9b02557f06b39911d833cb41
Carlos <loucerac@unican.es>
2018-01-26 10:29:15 +0100

    sif writing simplified by making use of 'toSif' 'outputFile' argument

--- rdiseasemaps/scr01.R
+++ rdiseasemaps/scr01.R
@@ -9,12 +9,9 @@ biopax_filepath <- normalizePath(
     file.path(config$folders$dna_folder, config$folders$biopax_filename)
 )
 
-# convert to sif
-sif <- toSif(inputFile = biopax_filepath)
-
-# write SIF
-sif_file_path <- paste0(biopax_filepath, ".sif") # already normalized
-write.table(sif, file = sif_file_path, sep = "\t", row.names = F, quote = F)
+# convert adn write to sif
+sif_filepath <- paste0(biopax_filepath, ".sif") # already normalized
+sif <- toSif(inputFile = biopax_filepath, outputFile = sif_filepath)
 
 # convert to igraph
 sif_graph <- graph.edgelist(as.matrix(sif[, c(1, 3)]), directed = T)
