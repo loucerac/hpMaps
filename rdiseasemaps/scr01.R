bc2705d954dcfd30b7c1baa4964caa0fa5cae127
Carlos <loucerac@unican.es>
2018-01-26 10:19:56 +0100

    write converted sif to data folder

--- rdiseasemaps/scr01.R
+++ rdiseasemaps/scr01.R
@@ -9,7 +9,13 @@ biopax_filepath <- normalizePath(
     file.path(config$folders$dna_folder, config$folders$biopax_filename)
 )
 
-sif <- toSif(inputFile=biopax_filepath)
+# convert to sif
+sif <- toSif(inputFile = biopax_filepath)
+
+# write SIF
+sif_file_path <- paste0(biopax_filepath, ".sif") # already normalized
+write.table(sif, file = sif_file_path, sep = "\t", row.names = F, quote = F)
+
 # convert to igraph
 sif_graph <- graph.edgelist(as.matrix(sif[, c(1, 3)]), directed = T)
 
